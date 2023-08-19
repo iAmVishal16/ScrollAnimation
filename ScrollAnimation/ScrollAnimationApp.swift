b409e75c9ca9e43380d01cc22fb673fb05413591
Vishal Paliwal <paliwalvishal16@gmail.com>
2023-08-19 21:28:25 +0530

    Initial Commit

---
+++ ScrollAnimation/ScrollAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollAnimationApp.swift
+//  ScrollAnimation
+//
+//  Created by Vishal Paliwal on 19/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
